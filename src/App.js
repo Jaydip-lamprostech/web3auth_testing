529eaffe75aaab4faeb3823b1f1f200c0326f0f1
Jaydip Patel <jaydip.lamprostech@gmail.com>
2023-05-08 18:02:14 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
-
+import { SafeEventEmitterProvider } from "@web3auth/base";
 import { Web3Auth } from "@web3auth/modal";
 import { ethers } from "ethers";
 import { useCallback, useEffect, useState } from "react";
+import Web3 from "web3";
+import contractABI from "./artifacts/contract.json";
+const contractAddress = "0x61De71734C89C9a359028962f6834A2ae099293e";
 
 //Initialize within your constructor
 const web3auth = new Web3Auth({
@@ -14,42 +17,44 @@ const web3auth = new Web3Auth({
   chainConfig: {
     chainNamespace: "eip155",
     chainId: "0x13881", // hex of 80001, polygon testnet
-    rpcTarget: "https://rpc.ankr.com/polygon_mumbai",
+    rpcTarget: "https://rpc-mumbai.maticvigil.com/",
     // Avoid using public rpcTarget in production.
     // Use services like Infura, Quicknode etc
-    displayName: "Polygon Mumbai Testnet",
-    blockExplorer: "https://mumbai.polygonscan.com/",
-    ticker: "MATIC",
-    tickerName: "Matic",
   },
   theme: "dark",
   web3AuthNetwork: "cyan",
 });
 
 function App() {
-  const [userData, setuserData] = useState({
+  const [userData, setUserData] = useState({
     name: "",
     profileImage: "",
     address: "",
     balance: "",
   });
+  const [msg, setMsg] = useState({ inputMsg: "", responseMsg: "" });
+  const [sendMsgText, setSendMsgText] = useState("Send Msg");
   const [loggedIn, setLoggedIn] = useState(false);
+  const [loginInstance, setLoginInstance] = useState();
   const [address, setAddress] = useState("");
 
   // web3auth code
 
   //initialize web3auth at the rendering of the app
   const initializeWeb3Auth = useCallback(async () => {
-    const data = await web3auth.initModal();
-    console.log(data);
+    await web3auth.initModal();
+    // console.log(data);
   }, []);
 
   // login function
   const login = async () => {
     try {
       const login = await web3auth.connect();
+      console.log(login);
       if (login) {
+        setLoginInstance(login);
         const provider = new ethers.providers.Web3Provider(login);
+        console.log(provider);
         const signer = provider.getSigner();
 
         // Get user's Ethereum public address
@@ -61,7 +66,7 @@ function App() {
         );
         const data = await web3auth.getUserInfo();
         console.log(data);
-        setuserData({
+        setUserData({
           name: data.name,
           profileImage: data.profileImage,
           email: data.email,
@@ -75,13 +80,126 @@ function App() {
       console.log(err);
     }
   };
+  // contract instance
 
+  const updateMsg = async () => {
+    try {
+      setSendMsgText("Sending...");
+      // const login = await web3auth.connect();
+      // const provider = new ethers.providers.Web3Provider(loginInstance);
+      // console.log(provider);
+      // const signer = provider.getSigner();
+      // console.log(signer);
+      // const contract = new ethers.Contract(
+      //   contractAddress,
+      //   contractABI,
+      //   signer
+      // );
+      const web3 = new Web3(loginInstance);
+      const contract = new web3.eth.Contract(contractABI, contractAddress);
+      const fromAddress = (await web3.eth.getAccounts())[0];
+      const response = await contract.methods
+        .sayHello(msg.inputMsg)
+        .send(
+          {
+            from: fromAddress,
+          },
+          function (error, transactionHash) {
+            if (transactionHash) {
+              console.log(transactionHash);
+
+              setSendMsgText("Sent");
+              setTimeout(() => {
+                setSendMsgText("Send Msg");
+                setMsg({ inputMsg: "", responseMsg: "" });
+              }, 2000);
+              // setApproveCase(3);
+            } else {
+              console.log(error);
+            }
+          }
+        )
+        .on("receipt", async function (receipt) {
+          console.log(receipt);
+        })
+        .on("error", async function (error) {
+          console.log(error);
+        });
+      console.log(response);
+
+      // const receipt = await contract.methods.sayHello("NEW_MESSAGE").send({
+      //   from: fromAddress,
+      //   maxPriorityFeePerGas: "500000000000", // Max priority fee per gas
+      //   maxFeePerGas: "60000000000000", // Max fee per gas
+      // });
+      // console.log(receipt);
+      // Send transaction to smart contract to update message
+      // const tx = await contract.sayHello("NEW_MESSAGE");
+      // // Wait for transaction to finish
+      // const receipt = await tx.wait();
+      // console.log(receipt);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const displayMsg = async () => {
+    try {
+      // const login = await web3auth.connect();
+      // const provider = new ethers.providers.Web3Provider(loginInstance);
+      // const signer = provider.getSigner();
+
+      // const contract = new ethers.Contract(
+      //   contractAddress,
+      //   contractABI,
+      //   signer
+      // );
+      const web3 = new Web3(loginInstance);
+      const contract = new web3.eth.Contract(contractABI, contractAddress);
+      console.log(contract);
+      // Send transaction to smart contract to update message
+      const message = await contract.methods.getHello().call();
+      // const data = await contract.getHello();
+      setMsg({ ...msg, responseMsg: message });
+      console.log(message);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const sendFunds = async () => {
+    try {
+      const login = web3auth.connect();
+      console.log(web3auth.provider);
+      // const provider = web3auth.provider;
+      const provider = new ethers.providers.Web3Provider(web3auth.provider);
+      const web3 = new Web3(loginInstance);
+      // console.log(web3);
+      const signer = provider.getSigner(login.address);
+      console.log(signer);
+      const destination = "0x408402F30618a6985c56cF9608E04CEA12CddC37";
+      const amount = ethers.utils.parseEther("0.01858");
+      const fromAddress = (await web3.eth.getAccounts())[0];
+      // console.log(fromAddress);
+      // Send transaction to smart contract to update message
+      const tx = await signer.sendTransaction({
+        from: fromAddress,
+        to: fromAddress,
+        value: amount,
+        maxPriorityFeePerGas: "5000000000", // Max priority fee per gas
+        maxFeePerGas: "6000000000000", // Max fee per gas
+      });
+      const receipt = await tx.wait();
+      console.log(receipt);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   // user information function after connection
   const getUserInfo = async () => {
     try {
       const data = await web3auth.getUserInfo();
       console.log("// connected user data");
-      setuserData(data);
+      // setuserData(data);
       console.log(data);
     } catch (err) {
       console.log(err);
@@ -112,6 +230,53 @@ function App() {
     }
   };
 
+  const getContractBalance = async () => {
+    try {
+      const web3 = new Web3(loginInstance);
+      const contract = new web3.eth.Contract(contractABI, contractAddress);
+      console.log(contract);
+      // Send transaction to smart contract to update message
+      const balance = await contract.methods.contractBalance().call();
+      console.log(balance);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const sendIntoContract = async () => {
+    try {
+      const web3 = new Web3(loginInstance);
+      console.log(web3);
+      const contract = new web3.eth.Contract(contractABI, contractAddress);
+      const fromAddress = (await web3.eth.getAccounts())[0];
+      console.log(fromAddress);
+      // const amount = ethers.utils.parseEther("0.00858");
+      const response = await contract.methods
+        .sendInContract(100000000)
+        .send(
+          {
+            from: fromAddress,
+          },
+          function (error, transactionHash) {
+            if (transactionHash) {
+              console.log(transactionHash);
+            } else {
+              console.log(error);
+            }
+          }
+        )
+        .on("receipt", async function (receipt) {
+          console.log(receipt);
+        })
+        .on("error", async function (error) {
+          console.log(error);
+        });
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
     initializeWeb3Auth();
     console.log("inside the useEffect");
@@ -122,15 +287,31 @@ function App() {
       <header className="App-header">
         <div className="button-grp">
           <button onClick={login}>Login</button>
-          <button className="getUserInfo" onClick={getUserInfo}>
+          {/* <button className="getUserInfo" onClick={getUserInfo}>
             Get User Info
           </button>
           <button className="authenticateUser" onClick={authenticateUser}>
             authenticateUser
-          </button>
+          </button> */}
           <button className="logout" onClick={logout}>
             logout
           </button>
+        </div>
+        <div className="msg">
+          <input
+            type="text"
+            value={msg.inputMsg ? msg.inputMsg : ""}
+            onChange={(e) => setMsg({ ...msg, inputMsg: e.target.value })}
+          />
+          <button onClick={() => updateMsg()}>{sendMsgText}</button>
+        </div>
+        <div className="msg">
+          <button onClick={() => displayMsg()}>Get Msg</button>
+          {msg.responseMsg ? <p>{msg.responseMsg}</p> : <p>-</p>}
+        </div>
+        <div className="msg">
+          <button onClick={getContractBalance}>Get Contract Balance</button>
+          <button onClick={sendIntoContract}>sendIntoContract</button>
         </div>
         <table className="user-info-table">
           <thead>
